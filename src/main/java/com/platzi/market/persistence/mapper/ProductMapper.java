9fd6ee7d1466122f0ee37a1cd61ad7cf4e84dd20
Oscar España <espaaoscar@gmail.com>
2021-02-01 17:53:10 -0500

    cambiar los métodos de ProductoRepository para que usen ProductMapper

--- src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -23,7 +23,7 @@ public interface ProductMapper {
     })
     Product toProduct(Producto producto);
 
-    List<Product> toProducts(List<Product> products);
+    List<Product> toProducts(List<Producto> productos);
 
     @InheritInverseConfiguration
     @Mapping(target = "codigoBarras", ignore = true)
